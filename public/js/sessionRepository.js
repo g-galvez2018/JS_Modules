5e8f2737f17f32efa9a7e7b659489b4db1330bc7
Gustavo Galvez <gustavo.galvez09@gmail.com>
2020-05-01 00:03:34 -0400

    first commit

---
+++ public/js/sessionRepository.js
@@ -0,0 +1,20 @@
+import {sessionTemplate} from './template'
+const sessionURL = 'session.json';
+
+let sessionList = [];
+
+function getSessions(){
+  return new Promise(function(resolve, reject){
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function (e) {
+      sessionList = e.target.response;
+      resolve(sessionList);
+    };
+    oReq.open('GET', sessionURL, true);
+    oReq.responseType = 'json';
+    oReq.send();
+  })
+}
+
+export {getSessions as default, sessionURL};
+export {sessionTemplate} from './template.js'
